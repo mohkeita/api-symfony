5f59db3ae3d2b56f3dbef2f133867d3e2e69f0d4
mohkeita <mohkeita13@gmail.com>
2020-01-17 00:04:28 +0100

    create request get all posts function

--- src/Controller/ApiPostController.php
+++ src/Controller/ApiPostController.php
@@ -2,18 +2,17 @@
 
 namespace App\Controller;
 
+use App\Repository\PostRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
 
 class ApiPostController extends AbstractController
 {
     /**
-     * @Route("/api/post", name="api_post")
+     * @Route("/api/post", name="api_post_index", methods={"GET"})
      */
-    public function index()
+    public function index(PostRepository $postRepository)
     {
-        return $this->render('api_post/index.html.twig', [
-            'controller_name' => 'ApiPostController',
-        ]);
+        return $this->json($postRepository->findAll(), 200, [], ['groups'=>'post:read']);
     }
 }
--- src/Entity/Post.php
+++ src/Entity/Post.php
@@ -5,6 +5,7 @@ namespace App\Entity;
 use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Serializer\Annotation\Groups;
 
 /**
  * @ORM\Entity(repositoryClass="App\Repository\PostRepository")
@@ -15,21 +16,25 @@ class Post
      * @ORM\Id()
      * @ORM\GeneratedValue()
      * @ORM\Column(type="integer")
+     * @Groups("post:read")
      */
     private $id;
 
     /**
      * @ORM\Column(type="string", length=255)
+     * @Groups("post:read")
      */
     private $title;
 
     /**
      * @ORM\Column(type="text")
+     * @Groups("post:read")
      */
     private $content;
 
     /**
      * @ORM\Column(type="datetime")
+     * @Groups("post:read")
      */
     private $createdAt;
 
